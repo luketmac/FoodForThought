9646251b2725e985a3df47fa35c6329b9c8f1d6b
Luke Macintosh <luketmacintosh@gmail.com>
2026-04-14 08:59:13 -0400

    Initial Commit

---
+++ FoodForThought/FoodForThoughtApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodForThoughtApp.swift
+//  FoodForThought
+//
+//  Created by Luke Macintosh on 4/14/26.
+//
+
+import SwiftUI
+
+@main
+struct FoodForThoughtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
